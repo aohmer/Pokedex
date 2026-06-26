8f397913d47f2e417361bd6b40a5c9d262ab31a0
aohmer <aaron.ohmer@gmail.com>
2026-06-26 19:52:36 -0400

    setup app added prompt and input

---
+++ src/repl.ts
@@ -0,0 +1,45 @@
+
+import { State } from "./state.js";
+
+export function startREPL(state: State) {
+  const rl = state.readline;
+
+  rl.prompt();
+
+  rl.on("line", async (input) => {
+    const words = cleanInput(input);
+    if (words.length === 0) {
+      rl.prompt();
+      return;
+    }
+
+    const commandName = words[0];
+
+    
+    const cmd = state.commands[commandName];
+    if (!cmd) {
+      console.log(
+        `Unknown command: "${commandName}". Type "help" for a list of commands.`,
+      );
+      rl.prompt();
+      return;
+    }
+
+    try {
+      cmd.callback(state);
+    } catch (e) {
+      console.log(e);
+    }
+
+    rl.prompt();
+  });
+}
+
+export function cleanInput(input: string): string[] {
+  return input
+    .toLowerCase()
+    .trim()
+    .split(" ")
+    .filter((word) => word !== "");
+}
+
